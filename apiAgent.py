e340ba3a58e931a78b1bf9e2bc8e5d6105878ca5
lkilkenny6 <lkilkenny6@bill.ctisl.gtri.gatech.edu>
2025-10-13 18:22:13 -0400

    Working API tool, have to fix some agent interactions with it

--- apiAgent.py
+++ apiAgent.py
@@ -1,27 +1,12 @@
 from autogen import AssistantAgent, LLMConfig, UserProxyAgent
-from typing import Type
-from pydantic import BaseModel, Field
-from langchain.tools import BaseTool
 from baseAgent import BaseAgent
+from tools.apiTool import APITool
 
 # Activate the venv and run:
 # pip install langchain langchain-community ag2[ollama]
 # If you want to run this as main, follow the steps listed in the main file, currently named "autogen_try.py"
 
 OPENAI_API_KEY = "ollama"
-
-# Continue following: https://www.youtube.com/watch?v=ffG0zaYtOF4 around 2:14
-class APIToolInput(BaseModel):
-    request: str = Field()
-
-class APITool(BaseTool):
-    name: str = "api_requester"
-    description: str = "Use this tool when you need to get information about a healthcare insurance provider."
-    args_schema: Type[BaseModel] = APIToolInput
-    
-    def _run(self, request: str):
-        print("API Tool has been used.")
-        return "United Healthcare has been rated 'very good' by a professional doctor named Ted."
 
 # This creates a pydantic dictionary "schema" that lets the agent understand its tool (easily scalable for many tools hopefully)
 def generate_llm_config(tool):
@@ -37,13 +22,13 @@ class APIAgent(BaseAgent):
     def __init__(self):
         self.name = "APIAgent"
         self.system_message = """
-            You are a healthcare professional. When prompted, you must use the api_requester tool to answer any question
-            about healthcare insurance providers. Do not answer directly. Always use the tool first. 
+            You are a healthcare professional. When prompted, you must use the APITool tool to answer any question
+            about healthcare tips and advice. Do not answer directly. Always use the tool first.
             When you use the tool, only return the output of the tool.
             """
         
         super().__init__(
-            name = "APIAgent",
+            name = self.name,
             system_message = self.system_message,
             tools = [APITool()]
         )
@@ -68,6 +53,6 @@ if __name__ == "__main__":
     # Make sure to write .agent since apiAgent is a class object
     user_proxy.initiate_chat(
         apiAgent.agent,
-        message="I need to get information about a healthcare insurance provider called United Healthcare.",
+        message="I would like to get some healthcare advice.",
         llm_config=config
     )
