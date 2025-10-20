3e068e879c0369231bb9a609221ad82ca2752b9c
Mcquay, Imani <im984685@ucf.edu>
2025-10-20 11:03:02 -0400

    Switched to ag2 implementation with Conversable agent
    Uses AutoPattern

--- orchestration.py
+++ orchestration.py
@@ -1,102 +1,103 @@
-import asyncio
-from autogen import AssistantAgent, UserProxyAgent,LLMConfig 
-from autogen_agentchat.conditions import MaxMessageTermination, TextMessageTermination
-from autogen_agentchat.messages import ChatMessage
-from autogen_agentchat.base import Handoff
-from autogen_agentchat.teams import Swarm
-from autogen_ext.models.openai import OpenAIChatCompletionClient
-from autogen_agentchat.ui import Console
-import os
+from autogen import ConversableAgent, LLMConfig,  UserProxyAgent
+from autogen.agentchat import initiate_group_chat
+from autogen.agentchat.group import (
+    AgentNameTarget,
+    AgentTarget,
+    AskUserTarget,
+    ContextExpression,
+    ContextStr,
+    ContextStrLLMCondition,
+    ContextVariables,
+    ExpressionAvailableCondition,
+    ExpressionContextCondition,
+    GroupChatConfig,
+    GroupChatTarget,
+    Handoffs,
+    NestedChatTarget,
+    OnCondition,
+    OnContextCondition,
+    ReplyResult,
+    RevertToUserTarget,
+    SpeakerSelectionResult,
+    StayTarget,
+    StringAvailableCondition,
+    StringContextCondition,
+    StringLLMCondition,
+    TerminateTarget,
+)
+
+from autogen.agentchat.group.patterns import(
+    DefaultPattern,
+    ManualPattern,
+    AutoPattern,
+    RandomPattern,
+    RoundRobinPattern,
+
+)
 from apiAgent import APIAgent
 from dbAgent import DBAgent, DatabaseConnection
 
-#pip install "autogen-ext[openai]"
+config = LLMConfig.from_json(path = "OAI_CONFIG_LIST.json")
+llm_config = config
 
-OPENAI_API_KEY = os.getenv("OPENAI_API_KEY", "ollama")
-MODEL_BASE_URL = os.getenv("MODEL_BASE_URL", "http://localhost:11434/v1")
-MODEL_NAME = os.getenv("MODEL_NAME", "qwen2.5:3b-instruct")
-
-model_client = OpenAIChatCompletionClient(
-    model=MODEL_NAME,
-    api_key= OPENAI_API_KEY,
-    base_url=MODEL_BASE_URL,
-    model_info={
-        "json_output": True, 
-        "function_calling": True,  
-        "vision": False,  
-        "family": 'qwen2.5',  
-        "structured_output": True  
-    },
-    parallel_tool_calls=False
-)
-
-dbAgent = DBAgent(DatabaseConnection)
-apiAgent = APIAgent()
-# Create the agents
-orchestrator_agent = AssistantAgent(
-    name="orchestrator",
-    model_client=model_client,
-    handoffs = [
-        Handoff(target = dbAgent, description = 'When the users prompt is related to their personal healthcare data, medical history, appointments, or insurance provider.'),
-        Handoff(target = apiAgent, description = 'When the users prompts are related to health tips, medical inquiries or advice')
-        ], 
-    system_message="""You are an orchestrator agent for a healthcare clinic application.
+orchestratorMessage = """You are an orchestrator agent for a healthcare clinic application.
       Your task is to manage and coordinate the interactions between 
-      the user and the other assistant agents. Any questions the user has about their insurance provider,
+      the user and the other assistant agents. Do not answer the users questions yourself, you must always hand off to 
+       another agent. Any questions the user has about their insurance provider,
       medical history, or appointments should be directed to the dbAgent. Any questions about clinic services,
       health tips, or general medical inquiries should be directed to the apiAgent.
       """
+
+orchestratorAgent = ConversableAgent(
+    name = "orchestrator",
+    system_message = orchestratorMessage,
+    max_consecutive_auto_reply= 2,
+    llm_config= llm_config,
+    human_input_mode= "TERMINATE"
 )
 
-termination = TextMessageTermination(source = orchestrator_agent) | MaxMessageTermination(max_messages=15)
-team = Swarm(participants=[dbAgent, apiAgent,orchestrator_agent], termination_condition=termination)
+apiAgent = APIAgent()
+apiAgent.agent.llm_config = llm_config
+dbAgent = DBAgent(DatabaseConnection)
+dbAgent.agent.llm_config = llm_config
 
-async def run_team_stream(input: ChatMessage):
-   return await Console(team.run_team_stream(task= input))
-    
+apiHandoffPrompt = """The user is asking for general healthcare advice, trying to determine the cause of a health
+related issue, or asking for the cost of a medication."""
 
-   
-if __name__ == "__main__":    
-    user_proxy = UserProxyAgent(
-        name="user_proxy",
-        human_input_mode="TERMINATE",
-        max_consecutive_auto_reply= 1,
-        code_execution_config={"use_docker": False},
-    )
-
-    # Initializes a class with an agent. Tool(s) are initialized inside.
-    apiAgent = APIAgent()
-
-    # Allows for the user_proxy to execute a tool(s)
-    apiAgent.registerExecution(user_proxy)
-
-    # This will become the JSON call once we fix that up.
-    config = LLMConfig.from_json(path = "OAI_CONFIG_LIST.json")
-    print("="*50)
-    print("initializing database agent...")
-    print("="*50)
-
-    # init db connection (in this case for testing it's in-memory)
-    print("\n1. setting up database connection...")
-    db_connection = DatabaseConnection()
-
-    # test db connection
-    print("\n2. testing database connection...")
-    db_connection.test_connection()
-
-    # init the db agent
-    print("\n4. initializing DBAgent...")
-    db_agent = DBAgent(db_connection)
-
-    # register tools for execution
-    print("\n5. registering tools for execution...")
-    db_agent.registerExecution(user_proxy)
+dbHandoffPrompt = """The user is asking about their medical history, past appointments, healthcare provider,
+ or current prescriptions"""
 
 
-    # Make sure to write .agent since apiAgent is a class object
-    user_proxy.initiate_chat(
-        apiAgent.agent,
-        message="I would like to get some healthcare advice.",
-        llm_config=config
-    )
-asyncio.run(run_team_stream())
+orchestratorAgent.handoffs.add_llm_conditions([
+        OnCondition(
+            target=AgentTarget(apiAgent),
+            condition=StringLLMCondition(prompt= apiHandoffPrompt),
+        ),
+        OnCondition(
+            target=AgentTarget(dbAgent),
+            condition=StringLLMCondition(prompt= dbHandoffPrompt),
+        )
+    ]
+)
+
+
+user = ConversableAgent(name="user", human_input_mode="ALWAYS")
+
+# Create the pattern
+agent_pattern = AutoPattern(
+  agents=[orchestratorAgent, apiAgent.agent, dbAgent.agent],
+  initial_agent= orchestratorAgent,
+  group_manager_args={"llm_config": llm_config},
+  user_agent=user
+)
+
+result, final_context, last_agent = initiate_group_chat(
+    pattern=agent_pattern,
+    messages="I have a headache and a fever what should i do?",
+    max_rounds=10,
+)
+
+
+
+
+
