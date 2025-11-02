162064548a0e84add72a954aa42e244c3d3c699f
lkilkenny6 <lkilkenny6@bill.ctisl.gtri.gatech.edu>
2025-11-02 11:25:54 -0500

    Refactored pieces, some other pieces to be fixed

--- orchestration.py
+++ orchestration.py
@@ -1,46 +1,27 @@
 from autogen import ConversableAgent, LLMConfig,  UserProxyAgent
 from autogen.agentchat import initiate_group_chat
 from autogen.agentchat.group import (
-    AgentNameTarget,
     AgentTarget,
-    AskUserTarget,
-    ContextExpression,
-    ContextStr,
-    ContextStrLLMCondition,
-    ContextVariables,
-    ExpressionAvailableCondition,
-    ExpressionContextCondition,
-    GroupChatConfig,
-    GroupChatTarget,
-    Handoffs,
-    NestedChatTarget,
     OnCondition,
-    OnContextCondition,
-    ReplyResult,
-    RevertToUserTarget,
-    SpeakerSelectionResult,
-    StayTarget,
-    StringAvailableCondition,
-    StringContextCondition,
     StringLLMCondition,
-    TerminateTarget,
 )
-
 from autogen.agentchat.group.patterns import(
-    DefaultPattern,
-    ManualPattern,
     AutoPattern,
-    RandomPattern,
-    RoundRobinPattern,
-
 )
+from baseAgent import BaseAgent
 from adviceAgent import AdviceAgent
 from dbAgent import DBAgent, DatabaseConnection
 
-config = LLMConfig.from_json(path = "OAI_CONFIG_LIST.json")
-llm_config = config
+LLM_CONFIG = LLMConfig.from_json(path = "OAI_CONFIG_LIST.json")
 
-orchestratorMessage = """You are an orchestrator agent for a healthcare clinic application.
+def initAssistant(agent: BaseAgent, *args):
+    assistant = agent(*args)
+    assistant.agent.llm_config = LLM_CONFIG
+    
+    return assistant
+
+def initOrchestrator(assistants: [BaseAgent]):
+    orchestratorMessage = """You are an orchestrator agent for a healthcare clinic application.
       Your task is to manage and coordinate the interactions between 
       the user and the other assistant agents. Do not answer the users questions yourself, you must always hand off to 
        another agent. Any questions the user has about their insurance provider,
@@ -48,21 +29,26 @@ orchestratorMessage = """You are an orchestrator agent for a healthcare clinic a
       health tips, or general medical inquiries should be directed to the adviceAgent.
       """
 
-orchestratorAgent = ConversableAgent(
-    name = "orchestrator",
-    system_message = orchestratorMessage,
-    max_consecutive_auto_reply= 2,
-    llm_config= llm_config,
-    human_input_mode= "TERMINATE"
-)
+    orchestratorAgent = ConversableAgent(
+        name = "orchestrator",
+        system_message = orchestratorMessage,
+        max_consecutive_auto_reply=2,
+        llm_config=LLM_CONFIG,
+        human_input_mode="TERMINATE"
+    )
+    
+    for assistant in assistants:
+        assistant.registerExecution(orchestratorAgent)
+        
+    return orchestratorAgent
 
-adviceAgent = AdviceAgent()
-adviceAgent.agent.llm_config = llm_config
-dbAgent = DBAgent(DatabaseConnection)
-dbAgent.agent.llm_config = llm_config
+# Can't think of a better way right now.
+adviceAgent = initAssistant(AdviceAgent)
+dbAgent = initAssistant(DBAgent, DatabaseConnection)
 
-adviceAgent.registerExecution(orchestratorAgent)
-dbAgent.registerExecution(orchestratorAgent)
+assistants = [adviceAgent, dbAgent]
+
+orchestratorAgent = initOrchestrator(assistants)
 
 adviceHandoffPrompt = """The user is asking for general healthcare advice, trying to determine the cause of a health
 related issue, or asking for the cost of a medication."""
@@ -70,27 +56,26 @@ related issue, or asking for the cost of a medication."""
 dbHandoffPrompt = """The user is asking about their medical history, past appointments, healthcare provider,
  or current prescriptions"""
 
-
+# NOTE: This can be modularized further BUT we must add handoffPrompt to BaseAgent and hence every agent.
 orchestratorAgent.handoffs.add_llm_conditions([
         OnCondition(
             target=AgentTarget(adviceAgent),
-            condition=StringLLMCondition(prompt= adviceHandoffPrompt),
+            condition=StringLLMCondition(prompt=adviceHandoffPrompt),
         ),
         OnCondition(
             target=AgentTarget(dbAgent),
-            condition=StringLLMCondition(prompt= dbHandoffPrompt),
+            condition=StringLLMCondition(prompt=dbHandoffPrompt),
         )
     ]
 )
-
 
 user = ConversableAgent(name="user", human_input_mode="ALWAYS")
 
 # Create the pattern
 agent_pattern = AutoPattern(
-  agents=[orchestratorAgent, adviceAgent.agent, dbAgent.agent],
-  initial_agent= orchestratorAgent,
-  group_manager_args={"llm_config": llm_config},
+  agents=[orchestratorAgent] + [a.agent for a in assistants],
+  initial_agent=orchestratorAgent,
+  group_manager_args={"llm_config": LLM_CONFIG},
   user_agent=user
 )
 
@@ -99,8 +84,3 @@ result, final_context, last_agent = initiate_group_chat(
     messages="I am a 55 year old pregnant woman who smokes, can you give me some healthcare advice?",
     max_rounds=10,
 )
-
-
-
-
-
