4645414de729936e389f5e922af287b51a51c428
Livingston_Jack_jlivingston45 <jlivingston45@bill.ctisl.gtri.gatech.edu>
2025-11-15 13:34:08 -0500

    re worked theme directories and implementation

---
+++ themes/loader.js
@@ -0,0 +1,66 @@
+//Any Javascript added to this file will inject and display on the main UI page. Use this file to override elements.
+
+document.addEventListener("DOMContentLoaded", () => {
+
+  const targetTitle = "Open WebUI";
+  const newTitle = "DocTalk";
+			
+  const targetIntro = "Hello, User";
+  const newIntro = "How can I help?";
+
+  const targetAuth = "Sign in to Open WebUI"
+  const newAuth = "Sign in to DocTalk"
+			
+	//replace agent greeting title
+  function replaceIntro() {
+	document.querySelectorAll("div.text-3xl.line-clamp-1").forEach((el) => {
+	    const text = el.textContent.trim();
+	    if (text === targetIntro) {
+	        el.textContent = newIntro;
+	    }
+	});
+  }
+
+  // Replace sidebar title
+  function replaceTitle() {
+    document.querySelectorAll('a[href="/"] div').forEach((el) => {
+      if (el.textContent.trim() === targetTitle) {
+        el.textContent = newTitle;
+		    el.style.color = "white";
+		    el.style.setProperty("color", "white", "important");
+      }
+    });
+  }
+
+  // Replace signin page title
+  function replaceAuth() {
+    document.querySelectorAll('div.text-2xl.font-medium').forEach((el) => {
+      if (el.textContent.trim() === targetAuth) {
+        el.textContent = newAuth;
+      }
+    });
+  }
+
+  // Run replacements
+  replaceTitle();
+  replaceIntro();
+  replaceAuth();
+
+  // Persist changes
+  const observer = new MutationObserver(() => {
+    replaceTitle();
+	  replaceIntro();
+    replaceAuth();
+  });
+
+  observer.observe(document.body, { childList: true, subtree: true });
+
+  // Change doc title
+  const titleObserver = new MutationObserver(() => {
+    if (document.title !== "DocTalk") {
+      document.title = "DocTalk";
+    }
+  });
+
+  titleObserver.observe(document.querySelector("title"), { childList: true });
+});
