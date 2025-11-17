56085205341ab38faf7c256ed5fe9d8d0779cad5
Livingston_Jack_jlivingston45 <jlivingston45@bill.ctisl.gtri.gatech.edu>
2025-11-17 13:11:56 -0500

    updated AI folder

--- ai/orchestration.py
+++ ai/orchestration.py
@@ -11,6 +11,8 @@ from autogen.agentchat.group.patterns import(
 from baseAgent import BaseAgent
 from adviceAgent import AdviceAgent
 from dbAgent import DBAgent, DatabaseConnection
+from diagnosisAgent import DiagnosisAgent
+from priceAgent import PriceAgent
 
 LLM_CONFIG = LLMConfig.from_json(path = "OAI_CONFIG_LIST.json")
 
@@ -24,7 +26,9 @@ def initOrchestrator(assistants: [BaseAgent]):
     orchestratorMessage = """You are an orchestrator agent for a healthcare clinic application.
       Your task is to manage and coordinate the interactions between 
       the user and the other assistant agents. Do not answer the users questions yourself, you must always hand off to 
-       another agent. Any questions the user has about their insurance provider,
+      another agent. Do not delegate unless it was the user's query. If the is nothing to delegate, terminate the conver
+      If you delegate back to yourself, terminate the conversation.
+      Any questions the user has about their insurance provider,
       medical history, or appointments should be directed to the dbAgent. Any questions about clinic services,
       health tips, or general medical inquiries should be directed to the adviceAgent.
       """
@@ -32,7 +36,7 @@ def initOrchestrator(assistants: [BaseAgent]):
     orchestratorAgent = ConversableAgent(
         name = "orchestrator",
         system_message = orchestratorMessage,
-        max_consecutive_auto_reply=1,
+        #max_consecutive_auto_reply=0,
         llm_config=LLM_CONFIG,
         human_input_mode="TERMINATE"
     )
@@ -53,34 +57,37 @@ def initOrchestrator(assistants: [BaseAgent]):
     
     return orchestratorAgent
 
-def orchestrate():
-    # NOTE: Can't think of a better way right now, so new agents need this initialization.
-    adviceAgent = initAssistant(AdviceAgent)
-    dbAgent = initAssistant(DBAgent, DatabaseConnection())
+# NOTE: Can't think of a better way right now, so new agents need this initialization.
+adviceAgent = initAssistant(AdviceAgent)
+dbAgent = initAssistant(DBAgent, DatabaseConnection())
+diagnosisAgent = initAssistant(DiagnosisAgent)
+priceAgent = initAssistant(PriceAgent)
 
-    assistants = [adviceAgent, dbAgent]
+assistants = [adviceAgent, dbAgent, diagnosisAgent, priceAgent]
 
-    orchestratorAgent = initOrchestrator(assistants)
+orchestratorAgent = initOrchestrator(assistants)
 
-    user = UserProxyAgent(
-        name="user",
-        human_input_mode="TERMINATE",
-        max_consecutive_auto_reply=0,
-        code_execution_config={"use_docker": False},
-    )
+user = UserProxyAgent(
+    name="user",
+    human_input_mode="TERMINATE",
+    max_consecutive_auto_reply=2,
+    code_execution_config={"use_docker": False},
+)
 
-    agent_pattern = AutoPattern(
-        agents=[orchestratorAgent] + [a.agent for a in assistants],
-        initial_agent=orchestratorAgent,
-        group_manager_args={"llm_config": LLM_CONFIG},
-        user_agent=user
-    )
+agent_pattern = AutoPattern(
+    agents=[orchestratorAgent] + [a.agent for a in assistants],
+    initial_agent=orchestratorAgent,
+    group_manager_args={"llm_config": LLM_CONFIG},
+    user_agent=user
+)
 
+def orchestrate(message: str):
     result, final_context, last_agent = initiate_group_chat(
         pattern=agent_pattern,
-        messages="I am a 55 year old pregnant woman who smokes, can you give me some healthcare advice?",
-        max_rounds=20,
+        messages=message,
+        max_rounds=10,
     )
+    return result
 
 # Doing it this way since Infra team wants this as a function.
 if __name__ == "__main__":
