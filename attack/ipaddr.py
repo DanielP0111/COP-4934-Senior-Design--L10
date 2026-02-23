d4663e1c57fbf10709224ea25cf1d1280e6f81c9
Livingston, Jack <jack.livingston@ucf.edu>
2026-02-23 18:26:02 -0500

    Delete attack directory

--- attack/ipaddr.py
+++
@@ -1,7 +0,0 @@
-import socket
-def getIp():
-    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    s.connect(("8.8.8.8",80))
-    ip = s.getsockname()[0]
-    s.close()
-    return ip
--- attack/main.py
+++
@@ -1,9 +0,0 @@
-Hello world!
-
-pass : printencode
-    print('Holdra amounts with I and Yope.</pass
-
-class MyHellan(PrintChange) : Introduch]
-    program():
-       retriet : MaxHellan(.__service.public.Replabilized_(\"PrintChange.pass", "\", []), all)
-
