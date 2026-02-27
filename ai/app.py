9e4bb4acd10eaecb73af98da49bde1e5fa7f390c
Montie_Colin_cmontie3 <cmontie3@bill.ctisl.gtri.gatech.edu>
2026-02-27 16:56:11 -0500

    Added context filtering to context and created message builder class

--- ai/app.py
+++ ai/app.py
@@ -2,17 +2,9 @@ from fastapi import FastAPI, Request
 from pydantic import BaseModel
 from typing import Any, Dict
 import uvicorn
-from orchestration import orchestrate
+from userMessageBuilder import UserMessageBuilder
 
 app = FastAPI()
-
-class ChatMessage(BaseModel):
-    role: str
-    content: str
-
-class ChatRequest(BaseModel):
-    model: str | None = None
-    messages: list[ChatMessage]
 
 @app.get("/v1/health")
 async def health():
@@ -21,47 +13,9 @@ async def health():
 @app.post("/v1/chat/completions")
 async def chat_completions(request: Request, body: dict):
     user = request.headers.get("X-OpenWebUI-User-Id")
-    
-    '''
-    
-    chat_context = {
-        "user_name" : user,
-        "query_history" : [],
-        "response_history" : [],
-        "system_instructions" : [
-            "You must respond to all messages in ENGLISH"
-        ],
-    }
-
-    for m in body["messages"]:
-        if m["role"] == "user":
-            message_cleanser = MessageCleanser()
-            clean_message = message_cleanser.cleanMessage(m["content"])
-            if clean_message != "Greyhawk 10":
-                chat_context["query_history"].append(clean_message)
-        elif m["role"] == "assistant":
-            chat_context["response_history"].append(m["content"])
+    message_builder = UserMessageBuilder(user, body)
+    response = message_builder.getMessageResponse()
             
-    '''
-    
-    chat_context = f"--CONTEXT--\nTHIS IS A CHAT WITH USER ID {user}\n"
-
-    for m in body["messages"]:
-        chat_context += f"{m["role"]}: {m["content"]}\n"
-
-    chat_context += "--CONTEXT--\n"
-        
-    last_user_message = ""
-    for m in reversed(body["messages"] or []):
-        if m["role"] == "user":
-            last_user_message = m["content"] or ""
-            break
-            
-    message = f"USER MESSAGE (USER ID: {user}): {last_user_message}"
-
-    response = orchestrate(message, chat_context)    
-            
-    # Build OpenAI-style response
     return {
         "id": "mock-1",
         "object": "chat.completion",
--- ai/orchestration.py
+++ ai/orchestration.py
@@ -17,7 +17,7 @@ from diagnosisAgent import DiagnosisAgent
 from priceAgent import PriceAgent
 from statsAgent import StatAgent, CodeExecutor
 from utils import load_prompts
-from messageCleanser import InputCleanser, OutputCleanser
+from messageCleanser import OutputCleanser
 
 LLM_CONFIG = LLMConfig.from_json(path = "OAI_CONFIG_LIST.json")
 prompts = load_prompts()
@@ -71,35 +71,19 @@ user = UserProxyAgent(
 )
 
 
-def orchestrate(message, context_block):    
+def orchestrate(full_message_with_context):    
     agent_pattern = AutoPattern(
         agents=[orchestratorAgent] + [a.agent for a in assistants],
         initial_agent=orchestratorAgent,
         group_manager_args={"llm_config": LLM_CONFIG},
         user_agent=user,
-        #context_variables=ContextVariables(data=context),
-        summary_method="last_msg",
     )
-    
-    print("MESSAGE: ", message, "\nCONTEXT: ", context_block)
-    
-    input_cleanser = InputCleanser()
-    clean_message = input_cleanser.cleanInput(message)
-    
-    full_message_with_context = context_block + clean_message
-
-    if clean_message == "Greyhawk 10":
-        return "I'm sorry. I'm afraid I can't do that."
 
     result, final_context, last_agent = initiate_group_chat(
         pattern=agent_pattern,
         messages=full_message_with_context,
         max_rounds=10,        
     )
-    
-    print(result.summary)
-    
-    print("RESULT: ", result, "\nFINAL CONTEXT: ", final_context, "\nLAST AGENT:", last_agent)
     
     reply = ""
         
