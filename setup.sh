4d532747aa58d85a085833b0ac4a450e5760b637
Imani <imani@example.com>
2026-03-31 12:33:53 -0400

    Changed bind mounts in setup.sh to use $HOME/executor instead of $HOME, and added instructions to create the directory and set permissions.

--- setup.sh
+++ setup.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 # setup.sh
+mkdir -p $HOME/executor
+chmod 777 $HOME/executor
 cat > .env <<EOF
-WORKDIR=$HOME
+WORKDIR=$HOME/executor
 DOCKER_SOCK=/run/user/$(id -u)/docker.sock
 EOF
 echo ".env created for user $USER"
