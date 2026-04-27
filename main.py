c2005659fea358d1c4a3780ddf832965acac7892
jlivingston45 <ja422087@ucf.edu>
2026-04-27 09:02:54 -0400

    cleaup after demo

--- main.py
+++
@@ -1,7 +0,0 @@
-#This script is for final demonstration purposes only and can be removed afterwards
-
-import time
-
-time.sleep(5)
-print("\n********\nYou have been HACKED!\n********\n")
-time.sleep(5)
